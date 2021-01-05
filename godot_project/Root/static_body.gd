7b670e7191be9010d64432b5c0c8cadb571b60ff
Tomasz Lisowski <t.p.lisowski@gmail.com>
2021-01-06 00:30:40 +0100

    Fix cyclic resource inclusion

--- godot_project/Root/static_body.gd
+++
@@ -1,5 +0,0 @@
-extends StaticBody
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	print_debug("The ground has been loaded")
--- godot_project/Tracks/track_test.gd
+++
@@ -1,5 +0,0 @@
-extends Spatial
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	print_debug("The track has been loaded")
