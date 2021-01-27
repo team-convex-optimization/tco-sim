f5cd2aeb53b03729b70b972e4fcb2f8309f20982
Tomasz Lisowski <t.p.lisowski@gmail.com>
2021-01-27 13:28:30 +0100

    Unpack car state to list of arguments when packing

--- testing/testController.py
+++ testing/testController.py
@@ -40,7 +40,7 @@ def writeShm(shm, sem, data):
     while i < 16:
         data[i + 2] = chStruct.pack(data[i + 2][0], data[i + 2][1])
         i += 1
-    data = controlStruct.pack(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10],data[11],data[12],data[13],data[14],data[15],data[16],data[17])
+    data = controlStruct.pack(*data)
     os.lseek(shm.fd, 0, os.SEEK_SET)
     sem.acquire()
     bytesWritten = 0
