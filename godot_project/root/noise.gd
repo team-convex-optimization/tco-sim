9b8a869d919e464e6acd98f09b33189c83b76537
Tomasz Lisowski <t.p.lisowski@gmail.com>
2021-03-30 14:29:15 +0200

    Improve friction simulation

--- godot_project/root/noise.gd
+++ godot_project/root/noise.gd
@@ -5,22 +5,15 @@ const noiseUpdate = 0.5 #seconds
 var time_elapsed = 0
 onready var randOffset = randi() #Ensure we get different images 
 onready var noise = OpenSimplexNoise.new()
+var txt = ImageTexture.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Configure noise params
 	noise.seed = randi()
-	noise.octaves = 1
-	noise.period = 5
-	noise.persistence = 0.01
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	time_elapsed += delta
-	if (time_elapsed > noiseUpdate):
-		noise.seed = randi() + randOffset
-		var txt = ImageTexture.new()
-		var img = noise.get_image(32, 18)
-		txt.create_from_image(img)
-		set_texture(txt)
-		time_elapsed = 0
+	noise.octaves = 4
+	noise.period = 20.0
+	noise.persistence = 0.8
+	var img = noise.get_image(640, 480)
+	txt.create_from_image(img)
+	set_texture(txt)
